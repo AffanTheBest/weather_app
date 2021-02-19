dda9594f0fcebef494ccf29dbdb4b8ce11bbbd5b
AffanTheBest <affan207278@gmail.com>
2021-02-19 11:43:13 +0530

    remove unwanted code

--- index.js
+++ index.js
@@ -10,31 +10,6 @@ const path = require("path");
 const { error } = require("console");
 const app = express();
 
-const homeFile = fs.readFileSync("home.html", "utf8");
-
-// const replaceValue = (tempValue, orgValue) => {
-//   // console.log(orgValue);
-//   if (orgValue.message != undefined) {
-//     let DataNotFound = tempValue.replace("{%tempVal%} °C", " ");
-//     DataNotFound = DataNotFound.replace("Min {%tempValMin%} °C | Max {%tempValMax%} °C"," ");
-//     DataNotFound = DataNotFound.replace("{%location%}", "Data Not Found!");
-//     DataNotFound = DataNotFound.replace(", {%country%}", " ");
-//     DataNotFound = DataNotFound.replace("{%tempstatus%}", " ");
-//     DataNotFound = DataNotFound.replace("{%weathericoncode%}", " ");
-//     DataNotFound = DataNotFound.replace("{%weather-name%}", " ");
-//     return DataNotFound;
-//   } else {
-//     let tempreature = tempValue.replace("{%tempVal%}", orgValue.main.temp);
-//     tempreature = tempreature.replace("{%tempValMin%}", orgValue.main.temp_min);
-//     tempreature = tempreature.replace("{%tempValMax%}", orgValue.main.temp_max);
-//     tempreature = tempreature.replace("{%location%}", orgValue.name);
-//     tempreature = tempreature.replace("{%country%}", orgValue.sys.country);
-//     tempreature = tempreature.replace("{%tempstatus%}",orgValue.weather[0].main);
-//     tempreature = tempreature.replace("{%weathericoncode%}",orgValue.weather[0].icon);
-//     tempreature = tempreature.replace("{%weather-name%}",orgValue.weather[0].main);
-//     return tempreature;
-//   }
-// };
 app.set('views', path.join(__dirname, 'Templates/views'));
 app.set('view engine','hbs');
 app.use(express.static(path.join(__dirname, 'public')))
